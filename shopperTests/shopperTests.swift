2a037e858b1291ce8ac6ed71a4de829488cf767e
hawk <danielcabri@gmail.com>
2018-03-06 09:18:05 +0100

    Unit test for Sign In, Out and create user

--- shopperTests/shopperTests.swift
+++ shopperTests/shopperTests.swift
@@ -9,10 +9,13 @@
 import XCTest
 import Firebase
 import FirebaseDatabase
+import FirebaseAuth
 
 @testable import shopper
 
 class shopperTests: XCTestCase {
+    let aEmail = "danielcabri@gmail.com"
+    let aPass = "123456"
     
     override func setUp() {
         super.setUp()
@@ -28,6 +31,50 @@ class shopperTests: XCTestCase {
         var ref: DatabaseReference!
         ref = Database.database().reference()
         XCTAssertNotNil(ref, "Es Nil")
+    }
+    
+    func testRegisterFirebase() {
+        let expect = expectation(description: "CreateFirebaseUser")
+        Auth.auth().createUser(withEmail: aEmail, password: aPass) { (user, error) in
+            guard error == nil else {
+                XCTFail("the auth has not be succesfully")
+                return
+            }
+            debugPrint(user?.email ?? "Not email")
+            debugPrint("Success creating user")
+            expect.fulfill()
+        }
+        
+        waitForExpectations(timeout: 10.0, handler: { (error) in
+            print("Error: \(String(describing: error?.localizedDescription))")
+        })
+    
+    }
+    
+    func testSignInFirebase()  {
+        let expect = expectation(description: "SignInUser")
+        Auth.auth().signIn(withEmail: aEmail, password: aPass) { (user, error) in
+            guard error == nil else {
+                XCTFail("the auth has not be succesfully")
+                return
+            }
+            debugPrint(user?.email ?? "Not email")
+            debugPrint("Success Sign In")
+            expect.fulfill()
+        }
+        
+        
+        waitForExpectations(timeout: 10.0, handler: { (error) in
+            print("Error: \(String(describing: error?.localizedDescription))")
+        })
+    }
+    
+    func testSignOutFirebase()  {
+        do {
+            try Auth.auth().signOut()
+        } catch  {
+            XCTFail("Fail going out")
+        }
     }
     
 }
