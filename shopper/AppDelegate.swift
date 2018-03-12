67d2e090fbc70b2925229e4cec79f5470ed31b1d
Misael Cuevas Vásquez <mcvasquez@users.noreply.github.com>
2018-03-12 14:27:59 +0100

    Added initial segues

--- shopper/AppDelegate.swift
+++ shopper/AppDelegate.swift
@@ -17,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        
+        UIApplication.shared.statusBarStyle = .lightContent
+        UINavigationBar.appearance().barStyle = .blackOpaque
+        
         return true
     }
 
---
+++ shopper/Scenes/Articles/ArticlesViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ArticlesViewController.swift
+//  shopper
+//
+//  Created by Misael Cuevas Vásquez on 3/12/18.
+//  Copyright © 2018 Cuevas Cabrera. All rights reserved.
+//
+
+import UIKit
+
+class ArticlesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        title = "Articulos"
+        
+        let userAccountItem = UIBarButtonItem(image: UIImage(named: "UserAccount"), style: .plain, target: self, action: nil)
+        self.navigationItem.rightBarButtonItem = userAccountItem
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+//        if #available(iOS 11.0, *) {
+//            navigationController?.navigationBar.prefersLargeTitles = true
+//            navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
+//        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+//        if #available(iOS 11.0, *) {
+//            navigationController?.navigationBar.prefersLargeTitles = false
+//        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    // MARK: - UITableView Delegates
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "articlesCell") as! ArticlesTableViewCell
+        cell.title.text = "Procucto 1"
+        cell.price.text = "$100.00"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "showArticleDetails", sender: nil)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
